20ae4aa55d4cf6480550455ff3655c40d34e658f
guillaume volgyesi <guillaumevolgyesi@gmail.com>
2019-02-16 17:33:44 +0100

    First commit

---
+++ Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour
+{
+    Rigidbody2D body;
+    float horizontal;
+    [SerializeField]float speed;
+    [SerializeField] float jumpForce;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horizontal * speed, body.velocity.y);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            body.velocity = body.velocity + Vector2.up * jumpForce;
+        }
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+}
